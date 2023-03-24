cd92e069c7458bac45182549b9e655842eaada8b
Sebastian Ramirez <110700982+sebastianpain@users.noreply.github.com>
2023-03-24 15:45:41 +0000

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,9 +4,9 @@ const NavBar = () => {
         <nav>
             <h1>Ecommerce</h1>
             <div>
-                <Link to='/item/Remeras'>Remeras</Link>
-                <Link to='/item/Pantalones'>Pantalones</Link>
-                <Link to='/item/Buzos'>Buzos</Link>
+                <Link to='/item1/Remeras'>Remeras</Link>
+                <Link to='/item2/Pantalones'>Pantalones</Link>
+                <Link to='/item3/Buzos'>Buzos</Link>
             </div>
         </nav>
     )
